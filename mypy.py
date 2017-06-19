161d7c9fb4f8c3dd0833c8bc74322281f0795bbb
siddharth ravichandran <sid.ravichandran@gmail.com>
2017-06-19 07:19:05 +0000

    Cloudformation template examples for use with ECS CLuster ALB/ELB and ASG for scaling instances and applications

---
+++ mypy.py
@@ -0,0 +1,37 @@
+from multiprocessing import Pool
+from multiprocessing import Process
+import os
+import sys
+import urllib.request
+sys.setrecursionlimit(2000)
+
+
+# def f(x):
+#     if x == 1 or x == 0:
+#         return 1
+#     else:
+#         return x * f(x - 1)
+#
+# name = "Siddharth"
+# age  = 33
+#
+# print("{0}, {1} is presently writing python code".format(name, age))
+# print("{0:_^6}".format("ola"))
+# print("{0:.3f}".format(1/3))
+def trigger10ktimes():
+    while True:
+        with urllib.request.urlopen('http://NodeECS90-EcsElb-1UM7C2OPPOZCB-1863189502.ap-southeast-1.elb.amazonaws.com') as response:
+            html = response.read()
+            print("{}-{}".format(os.getpid(), html))
+
+processes = []
+for i in range(10):
+    new_process = Process(target=trigger10ktimes)
+    processes.append(new_process)
+    new_process.start()
+    print("Launched: {}", new_process.pid)
+
+for process in processes:
+    process.join()
+
+
